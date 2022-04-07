c2cccf94ac463eb8e5d853137293ff55be3d5be5
Rob Mulder <hallo@robmulder.com>
2022-04-07 21:17:38 +0200

    GAIU

--- app/Http/Controllers/SourcesController.php
+++ app/Http/Controllers/SourcesController.php
@@ -20,10 +20,15 @@ class SourcesController extends BaseController
 
         $headers = $request->headers->all();        
         $appConfig = new AppConfigController();
-        
+     
+        // Check if x-appconfg-key key is equal to the AppConfig key
+        if (!array_key_exists('x-appconfg-key', $headers)) { return false; }
+        if ($headers['x-appconfg-key'][0] != $appConfig->get('x-appconfg-key')) { return false; }
+
+        // Check if x-f1-key key is equal to the AppConfig key
         if (!array_key_exists('x-f1-key', $headers)) { return false; }
         if ($headers['x-f1-key'][0] != $appConfig->get('x-f1-key')) { return false; }
 
         return true;
-    }
+    }    
 }
